959ed8a0d4952942557b66aee62a7894f8ef1e8e
Super-chen-big <2049515545@qq.com>
2023-05-04 22:36:49 +0800

    添加上示例

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,53 +1,43 @@
 <template>
-  <view class="index">
-    <view>
-      <img src="" alt="">
-    </view>
-    {{ msg }} <Dongdong />
-    <view class="btn">
-      <nut-button type="primary" @click="handleClick('text', msg2, true)">点我</nut-button>
-    </view>
-    <nut-toast :msg="msg2" v-model:visible="show" :type="type" :cover="cover"/>
-  </view>
+  <nut-date-picker
+      v-model="currentDate"
+      title="日期时间选择"
+      type="datetime"
+      :filter="filter"
+      :min-date="minDate"
+      :max-date="maxDate"
+      @confirm="confirm"
+  ></nut-date-picker>
 </template>
+<script lang="ts">
+import {reactive} from 'vue';
 
-<script>
-import { reactive, toRefs } from 'vue';
-import { Dongdong } from '@nutui/icons-vue-taro';
 export default {
-  name: 'Index',
-  components: {
-    Dongdong
-  },
   setup() {
-    const state = reactive({
-      msg: '欢迎使用 NutUI4.0 开发小程序',
-      msg2: '你成功了～',
-      type: 'text',
-      show: false,
-      cover: false
-    });
-
-    const handleClick = (type, msg, cover = false) => {
-      state.show = true;
-      state.msg2 = msg;
-      state.type = type;
-      state.cover = cover;
-    };
-
-    return {
-      ...toRefs(state),
-      handleClick
+    const currentDate = new Date(2022, 4, 10, 10, 10);
+    const classRoomData=reactive({
+      name:'一战成硕自习室',
+      isOpen:true,
+      startTime:'6:00',
+      endTime:'23:00',
+      address:'河南省郑州市管城回族区区商都路55号亚星投资大厦1510室',
+    })
+    const filter = (type: string, options) => {
+      if (type == 'hour') {
+        return options.filter((option) => {
+          return Number(option.value)>=Number(classRoomData.startTime.split(':')[0])&&Number(option.value)<=Number(classRoomData.endTime.split(':')[0]);
+        });
+      }
+      return options;
     }
+    return {
+      currentDate,
+      // minDate: new Date(2020, 0, 1),
+      // maxDate: new Date(2025, 10, 1),
+      filter,
+      minDate:new Date(2023,4,4),
+      maxDate:new Date(2023,9,10)
+    };
   }
-}
+};
 </script>
-
-<style lang="scss">
-.index {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-}
-</style>
